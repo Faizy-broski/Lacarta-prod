8a7881eb1be0447c88aaf0c98fc53ec0f22f5c19
MhuzaifaWebDev <faizanhashmi603@gmail.com>
2026-04-01 19:00:40 +0500

    auth  build setup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { SignIn } from '@/features/auth/sign-in/index'
 export default SignIn
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,4 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { SignUp } from '@/features/auth/sign-up/index'
 export default SignUp
