2d984e7f3cad541e484017e19e8e0d4bb372bdc8
MhuzaifaWebDev <faizanhashmi603@gmail.com>
2026-04-10 21:30:51 +0500

    updated listing routes

--- app/(dashboard)/dashboard/listings/[id]/edit/page.tsx
+++ app/(dashboard)/dashboard/listings/[id]/edit/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { Suspense } from 'react'
 import { ListingFormPage } from '@/features/listings/ListingFormPage'
 
--- app/(dashboard)/dashboard/listings/[id]/page.tsx
+++ app/(dashboard)/dashboard/listings/[id]/page.tsx
@@ -1,3 +1,4 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { ListingDetailPage } from '@/features/listings/ListingDetailPage'
 export default ListingDetailPage
